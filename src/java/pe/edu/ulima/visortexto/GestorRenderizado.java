423a21658933acb2c66a45455db7702db5762907
W5209 <W5209@lab.aca>
2016-10-03 09:46:51 -0500

    patron singleton

--- src/java/pe/edu/ulima/visortexto/GestorRenderizado.java
+++ src/java/pe/edu/ulima/visortexto/GestorRenderizado.java
@@ -4,8 +4,22 @@ package pe.edu.ulima.visortexto;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
+
 public class GestorRenderizado {
     
+    private static GestorRenderizado singleton=null;
+    
+    public static GestorRenderizado getInstance(){
+        if (singleton==null){
+            singleton= new GestorRenderizado();
+        }
+         return singleton;
+    }
+
+    private GestorRenderizado() {
+    }
+
+
     public ByteArrayOutputStream renderizar(String titulo, String contenido, String tipo) throws IOException{
          
         ModoVisualizacionFactory factory=new ModoVisualizacionFactory();
